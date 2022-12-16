8ed0d4407875d7c3ca0979533392788a5900df56
Darkman_1421 <tamrazov00@bk.ru>
2022-12-16 18:10:21 +0300

    If/Else

---
+++ Desktop/Fgk/Example005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Write username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "tom")
+{
+    Console.WriteLine("Yeeee, it's Tom!!!");
+}
+else
+{
+    Console.Write("Hi bro!");
+    Console.Write(username);
+}
