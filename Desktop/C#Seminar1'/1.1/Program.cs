82d58d0826ad1f2d0bbf2af0cc59dbeae419b49b
Darkman_1421 <tamrazov00@bk.ru>
2022-12-16 22:42:20 +0300

    1.1

---
+++ Desktop/C#Seminar1'/1.1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter number 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Enter number 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == Math.Pow(b, 2))
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
