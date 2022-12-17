81f4fbc6642aefb8ff13ca2658830f35bac75a2f
Darkman_1421 <tamrazov00@bk.ru>
2022-12-17 18:14:43 +0300

    Таблица умножения

---
+++ Desktop/C#Seminar1'/0.4/Program.cs
@@ -0,0 +1,8 @@
+﻿//Таблица умножения
+Console.WriteLine("Введите число для выведения таблицы умножения");
+
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i<11; i++)
+{
+    Console.Write(a + "*" + i + " = "); Console.WriteLine(a*i);
+}
