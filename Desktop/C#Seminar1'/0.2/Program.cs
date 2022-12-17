110dc0e5e3c36d9d83ad43f7abeaccc301130a57
Darkman_1421 <tamrazov00@bk.ru>
2022-12-17 15:54:41 +0300

    Целые числа от -Х до Х

---
+++ Desktop/C#Seminar1'/0.2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показываем все целые числа в промежутке от -N до N
+Console.WriteLine("Enter a number: ");
+
+int A = Convert.ToInt32(Console.ReadLine());
+
+int index = A * -1;
+while(index <= A)
+{
+    System.Console.Write( index);
+    index++;
+}
