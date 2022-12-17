e1fecc173683716579a7b03de849b6ddc225300e
Darkman_1421 <tamrazov00@bk.ru>
2022-12-17 19:35:28 +0300

    Задача 6

---
+++ Desktop/C#Seminar1'/1.3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Чет-Нечет
+Console.Write("Введи число: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.Write("Yes)");
+}
+else
+{
+    Console.Write("NO!");
+}
