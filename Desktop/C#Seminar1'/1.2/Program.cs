5333f551c7bd11c5f6db52990736d2f896ea210f
Darkman_1421 <tamrazov00@bk.ru>
2022-12-17 15:18:06 +0300

    Задача 4

---
+++ Desktop/C#Seminar1'/1.2/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.Write("numbers1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("numbers2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("numbers3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max=a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c; 
+
+System.Console.Write("max = ");
+System.Console.Write(max);
