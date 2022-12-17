ee7b45d332900f2f8d250b40be51f47bf3600d68
Darkman_1421 <tamrazov00@bk.ru>
2022-12-17 17:27:10 +0300

    %%

---
+++ Desktop/C#Seminar1'/0.3/Program.cs
@@ -0,0 +1,7 @@
+﻿// Написать последнию цифрц трехзначного числа
+ int a = Convert.ToInt32(Console.ReadLine());
+
+int lastNumber= a % 10;
+
+System.Console.WriteLine(lastNumber);
+// Смысл в том, что берем число из остактка, после того, как поделили на 10
