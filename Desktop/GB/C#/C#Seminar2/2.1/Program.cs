4eb1ffc4c6a302e3fe3a1fe702411e94ce31a9ca
Darkman_1421 <tamrazov00@bk.ru>
2022-12-22 15:23:51 +0300

    Поиск числа в массиве

---
+++ Desktop/GB/C#/C#Seminar2/2.1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Из двух чисел
+Console.WriteLine("Enter first ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter second ");
+int b = int.Parse(Console.ReadLine());
+
+int max = Math.Max(a,  b);
+int min = Math.Min(a, b); 
+System.Console.WriteLine($"max num - {max} , {min} - min num");
